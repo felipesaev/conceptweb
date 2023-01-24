d53a75ce5cfdbabd6a6a17e9df959e7828b8aaed
Felipe Costa <felipesaev@gmail.com>
2023-01-23 21:44:06 -0300

    refactgor base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  base: '/concept/',
+  base: '/',
   build: {
     outDir: '../dist',
   },
